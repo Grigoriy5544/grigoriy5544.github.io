e62be7a5c362a2888463cb62571079ecf4fd2e6d
Grigoriy5544 <gregkotsenko@yandex.ru>
2023-08-14 18:07:35 +0400

    added steam library project

--- src/helpers/projectsList.js
+++ src/helpers/projectsList.js
@@ -1,35 +1,57 @@
-import musicology from "./../img/projects/Musicology.jpg"
-import musicologyBig from "./../img/projects/Musicology-big.jpg"
-import cryptoSever from "./../img/projects/crypto-sever.jpg"
-import cryptoSeverBig from "./../img/projects/crypto-sever-big.jpg"
-import psychoArt from "./../img/projects/psychoArt.jpg"
-import psychoArtBig from "./../img/projects/psychoArt-big.jpg"
+import musicology from './../img/projects/Musicology.jpg'
+import musicologyBig from './../img/projects/Musicology-big.jpg'
+import cryptoSever from './../img/projects/crypto-sever.jpg'
+import cryptoSeverBig from './../img/projects/crypto-sever-big.jpg'
+import psychoArt from './../img/projects/psychoArt.jpg'
+import psychoArtBig from './../img/projects/psychoArt-big.jpg'
+import cyberpunk from './../img/projects/cyberpunk.jpg'
+import cyberpunkBig from './../img/projects/cyberpunk-big.jpg'
+import steamLibrary from './../img/projects/steam-library.jpg'
+import steamLibraryBig from './../img/projects/steam-library-big.jpg'
 
 const projects = [
-    {
-        title: 'Musicology',
-        skills: 'HTML, CSS, JS',
-        img: musicology,
-        imgBig: musicologyBig,
-        gitHubLink: 'https://github.com/Grigoriy5544/Musicology',
-        url: "https://grigoriy5544.github.io/Musicology/",
-    },
-    {
-        title: 'Crypto Sever',
-        img: cryptoSever,
-        imgBig: cryptoSeverBig,
-        skills: 'HTML, CSS, JS',
-        gitHubLink: 'https://github.com/Grigoriy5544/crypto-sever',
-        url: "https://grigoriy5544.github.io/crypto-sever/"
-    },
-    {
-        title: 'PsychoArt',
-        img: psychoArt,
-        imgBig: psychoArtBig,
-        skills: 'React, TypeScript, SCSS',
-        gitHubLink: 'https://github.com/Grigoriy5544/psycho-art/',
-        url: "https://grigoriy5544.github.io/psycho-art/"
-    },
-];
+	{
+		title: 'PsychoArt',
+		img: psychoArt,
+		imgBig: psychoArtBig,
+		skills: 'React, TypeScript, SCSS',
+		gitHubLink: 'https://github.com/Grigoriy5544/psycho-art/',
+		url: 'https://grigoriy5544.github.io/psycho-art/',
+	},
+	{
+		title: 'Steam Library',
+		img: steamLibrary,
+		imgBig: steamLibraryBig,
+		skills: 'React, React Query, Node js, Express',
+		description:
+			"If you don't have a steam id, use, for example: 76561199093018035",
+		gitHubLink: 'https://github.com/Grigoriy5544/steam_library',
+		url: 'https://steam-library.vercel.app',
+	},
+	{
+		title: 'Cyberpunk',
+		img: cyberpunk,
+		imgBig: cyberpunkBig,
+		skills: 'HTML, CSS, JS',
+		gitHubLink: 'https://github.com/Grigoriy5544/cyberpunk',
+		url: 'https://grigoriy5544.github.io/cyberpunk/',
+	},
+	{
+		title: 'Musicology',
+		skills: 'HTML, CSS, JS',
+		img: musicology,
+		imgBig: musicologyBig,
+		gitHubLink: 'https://github.com/Grigoriy5544/Musicology',
+		url: 'https://grigoriy5544.github.io/Musicology/',
+	},
+	{
+		title: 'Crypto Sever',
+		img: cryptoSever,
+		imgBig: cryptoSeverBig,
+		skills: 'HTML, CSS, JS',
+		gitHubLink: 'https://github.com/Grigoriy5544/crypto-sever',
+		url: 'https://grigoriy5544.github.io/crypto-sever/',
+	},
+]
 
-export {projects}
+export { projects }
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,32 +1,39 @@
-import {useParams} from "react-router-dom";
-import BtnGitHub from "../components/btnGitHub/BtnGitHub";
-import {projects} from "../helpers/projectsList";
-
+import { useParams } from 'react-router-dom'
+import BtnGitHub from '../components/btnGitHub/BtnGitHub'
+import { projects } from '../helpers/projectsList'
 
 const Project = () => {
-    const {id} = useParams()
-    const project = projects[id]
+	const { id } = useParams()
+	const project = projects[id]
 
-    return (
-        <main className="section">
-            <div className="container">
-                <div className="project-details">
-                    <h1 className="title-1">{project.title}</h1>
-                    <a href={project.url} target="_blank" rel="noreferrer">
-                        <img src={project.imgBig} alt={project.title} className="project-details__cover"/>
-                    </a>
+	return (
+		<main className='section'>
+			<div className='container'>
+				<div className='project-details'>
+					<h1 className='title-1'>{project.title}</h1>
+					<a href={project.url} target='_blank' rel='noreferrer'>
+						<img
+							src={project.imgBig}
+							alt={project.title}
+							className='project-details__cover'
+						/>
+					</a>
 
-                    <div className="project-details__desc">
-                        <p>{project.title}</p>
-                    </div>
+					<div className='project-details__skills'>
+						<p>{project.skills}</p>
+					</div>
 
-                    {project.gitHubLink && (
-                        <BtnGitHub gitHubLink={project.gitHubLink}/>
-                    )}
-                </div>
-            </div>
-        </main>
-    );
-};
+					{project?.description && (
+						<div className='project-details__desc'>
+							<p>{project.description}</p>
+						</div>
+					)}
 
-export default Project;
+					{project.gitHubLink && <BtnGitHub gitHubLink={project.gitHubLink} />}
+				</div>
+			</div>
+		</main>
+	)
+}
+
+export default Project
