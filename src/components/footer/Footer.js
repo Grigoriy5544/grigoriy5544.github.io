8d36c1370c2f2733e7bd1c7e0665ad49db8bb270
Grigoriy5544 <90390215+Grigoriy5544@users.noreply.github.com>
2024-06-26 22:56:13 +0400

    Update Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -8,7 +8,6 @@ const Footer = () => {
             <div className="container">
                 <div className="footer__wrapper">
                     <ul className="social">
-                        <li className="social__item"><a href="https://vk.com/grigoriy5544" target="_blank" rel="noreferrer"><img src={vk} alt="Link"/></a></li>
                         <li className="social__item"><a href="https://github.com/Grigoriy5544" target="_blank" rel="noreferrer"><img src={gitHub} alt="Link"/></a></li>
                     </ul>
                 </div>
